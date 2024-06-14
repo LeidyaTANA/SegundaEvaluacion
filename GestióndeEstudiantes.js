38df5397195d5c5d18a5639f82c329c6f92d610c
Leidy Echeverri <yurleidyecheverriatanaka@gmail.com>
2024-06-14 11:59:31 -0600

    add nuevo codigo

--- GestióndeEstudiantes.js
+++ GestióndeEstudiantes.js
@@ -4,41 +4,47 @@
 // Requisitos:
 
 // 1. Define un objeto escuela con una propiedad estudiantes que sea un array vacío.
-// 2. Crea una función agregarEstudiante que reciba un nombre y un array de calificaciones, 
+// 2. Crea una función agregarEstudiante que reciba un nombre y un array de calificaciones,
 // y añada un objeto con estas propiedades al array estudiantes.
 // 3. Crea una función listarEstudiantes que imprima en consola cada estudiante y sus calificaciones.
 
-// 4. Crea una función promedioEstudiante que reciba el nombre de un estudiante y 
+// 4. Crea una función promedioEstudiante que reciba el nombre de un estudiante y
 // devuelva el promedio de sus calificaciones.
 
-let escuela = []
+let escuela = {
+    estudiantes: []
+}
 
 function AgregarEstudiante (nombre, calificaciones) {
     this.nombre = nombre;
     this.calificaciones = calificaciones;
 }
+// let estudiante2 = new AgregarEstudiante("Leidy", [30, 45, 35]);
+escuela.estudiantes.push(new AgregarEstudiante("Juan", [45, 50, 45]));
+escuela.estudiantes.push(new AgregarEstudiante("Leidy", [45, 50, 45]));
 
-let estudiante1 = new AgregarEstudiante("Juan", [45, 50, 45]);
-let estudiante2 = new AgregarEstudiante("Leidy", [30, 45, 35]);
-escuela.push(estudiante1, estudiante2);
+console.log(escuela.estudiantes);
 
+
+//
+//
 function listarEstudiantes(){
-    for(let i = 0; i < escuela.length; i++){
-        console.log(`Estudiante: ${escuela[i].nombre}, Calificaciones: ${escuela[i].calificaciones}`);
-    //console.log(inventario[i]);
+    for(let i = 0; i < escuela.estudiantes.length; i++){
+        console.log(`Estudiante: ${escuela.estudiantes[i].nombre}, Calificaciones: ${escuela.estudiantes[i].calificaciones}`);
     }
 }
+
+console.log(listarEstudiantes());
 
 function promedioEstudiante(nombre) {
-    
-    if (estudiante) {
-        let suma = escuela.calificaciones.reduce((acc, calificacion) => acc + calificacion, 0);
-        let promedio = suma / escuela.calificaciones.length;
-    
-    } else {
-        break;
+
+    if (escuela.estudiantes) {
+        let estudianteIndividual = escuela.estudiantes.find((estudiante) => estudiante.nombre === nombre)
+        let suma = estudianteIndividual.calificaciones.reduce((acc, calificacion) => acc + calificacion, 0);
+        let promedio = suma / estudianteIndividual.calificaciones.length;
+        return promedio;
     }
 }
-
-console.log(`Promedio de Juan: ${promedioEstudiante("Juan")}`);
-console.log(`Promedio de Leidy: ${promedioEstudiante("Leidy")}`);
+//
+console.log(`Promedio de Juan: ${promedioEstudiante("Leidy")}`);
+// console.log(`Promedio de Leidy: ${promedioEstudiante("Leidy")}`);
